9dd50eba999016461c50ae9c096febf7ee6920c0
manarm <mitchell.manar@lessannoyingcrm.com>
2023-03-09 10:48:18 -0600

    vars

---
+++ 0_basics_output.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+* Any text you see that is greyed out is a comment, and doesn't effect the code at all.
+* If you write code well, it will explain itself, but sometimes it's good to provide context.
+*/ 
+
+/*
+* The 'echo' keyword will print output when we run our code. This is how we will report results.
+* Note that in code, text is referred to as "strings", and is generally enclosed in quotes.
+* Echo can also take numbers.
+* Try editing these lines, and re-running the code!
+*/ 
+
+// This is a string and echoes text. '\n' is a special character that outputs a newline, like hitting Enter.
+echo "Hey there.\n";
+// This echoes a number.
+echo 42;
---
+++ 1_basics_variables.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+* Programs use "variables" to store and manipulate information.
+* In php, variables are marked with a '$' at the beginning.
+* Variables can be echo'd, and can hold lots of different types of information.
+*/
+
+/*
+* Variables can hold numbers and strings. 
+* We call values that are written directly into the program "constants", since they will never change no matter
+* how many times we run the program.
+*/
+$x = 3;
+echo $x;
+echo "\n";
+$y = "betelgeuse";
+echo $y;
+echo "\n";
+
+/*
+* Variables can also hold the results of expressions.
+* The general form looks like this:
+* 	$Variable = Expression
+* The thing on the right gets evaluated and stored in the variable on the left for use later.
+*/
+$x = 1 + 4;
+echo $x;
+echo "\n";
+// You can stick strings together; this is called "concatenation".
+// For some reason, this is done with a "." in php.
+$y = "Wow " . "mom";
+echo "$y"."\n";
+
+// And of course, expressions can also contain variables.
+$x = 19;
+$y = $x + 1;
+echo $y;
+echo "\n";
