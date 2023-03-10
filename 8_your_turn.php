39437342fa5001311c7327daed4a47a0d52dd88f
manarm <mitchell.manar@lessannoyingcrm.com>
2023-03-10 09:48:01 -0600

    tweak

--- 8_your_turn.php
+++ 8_your_turn.php
@@ -3,10 +3,12 @@ include_once('library.php');
 
 /*
 * Here's a blank file to do whatever you would like with.
-* If I could offer two major tips:
+* If I could offer some tips:
 * 1. Copy/paste -- don't reinvent the wheel! Feel free to liberally borrow from the previous exercises
 * 	 and only change what you need to.
-* 2. Ask for help -- the devs are here to give you a hand! Struggling is good for learning, but time
+* 2. Start simple -- do a small thing, make sure it works, and then add to it. Never assume what you
+*    have written will "just work", that is a recipe for sadness.
+* 3. Ask for help -- the devs are here to give you a hand! Struggling is good for learning, but time
 *    is limited and we'd love to help you get un-stuck or sketch out an idea.
 */
 
