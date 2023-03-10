50dda9b30a3bef22cbe4460e53081b4c84e102da
manarm <mitchell.manar@lessannoyingcrm.com>
2023-03-10 09:46:25 -0600

    project file

---
+++ 8_your_turn.php
@@ -0,0 +1,15 @@
+<?php
+include_once('library.php');
+
+/*
+* Here's a blank file to do whatever you would like with.
+* If I could offer two major tips:
+* 1. Copy/paste -- don't reinvent the wheel! Feel free to liberally borrow from the previous exercises
+* 	 and only change what you need to.
+* 2. Ask for help -- the devs are here to give you a hand! Struggling is good for learning, but time
+*    is limited and we'd love to help you get un-stuck or sketch out an idea.
+*/
+
+$FunctionName = 'GetUser';
+$Paramters = array();
+echo CallLacrmApi($FunctionName, $Paramters);
